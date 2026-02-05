5c99a330c9f860a5166463560909208bd616bd46
alexjch <alex.jch@gmail.com>
2026-02-05 19:26:41 +0000

    Adding devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+dnf install git python3 python3-pip -y
+python -m venv .venv
+echo 'source /workspace/.venv/bin/activate' >> ~/.bashrc
+.venv/bin/pip3 install --upgrade pip
+.venv/bin/pip3 install -r requirements.txt
+.venv/bin/pip3 install linux-mcp-server
