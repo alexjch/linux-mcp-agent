82b63faea518d24721d2c822d0c929f6eedb8075
alexjch <alex.jch@gmail.com>
2026-02-08 02:18:04 +0000

    Update devcontainer configuration
    - Added Containerfile to build container.
    - Removed setup.sh

--- .devcontainer/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-dnf install git python3 python3-pip make -y
-python -m venv .venv
-echo 'source /workspace/.venv/bin/activate' >> ~/.bashrc
-.venv/bin/pip3 install --upgrade pip
-.venv/bin/pip3 install -r requirements.txt
-.venv/bin/pip3 install linux-mcp-server
